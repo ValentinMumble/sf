ef9b74b1dc95f1ede7021ebefaae5d2a7c37728f
valentinmumble <valentindijkstra@gmail.com>
2019-09-07 18:04:02 +0200

    Add .htaccess rules in Apache.conf, implement HKController API endpoints

--- README.md
+++ README.md
@@ -3,8 +3,19 @@ Apache configuration
 ```lang-bsh
 Alias "/sf" "/var/www/html/hk/symfony-server/public/"
 <Directory "/var/www/html/hk/symfony-server/public/">
-  AllowOverride All
+  AllowOverride None
   Require all granted
   Allow from All
+  FallbackResource /index.php
+  RewriteEngine On
+  RewriteCond %{REQUEST_URI}::$1 ^(/.+)/(.*)::\2$
+  RewriteRule ^(.*) - [E=BASE:%1]
+  RewriteCond %{HTTP:Authorization} .
+  RewriteRule ^ - [E=HTTP_AUTHORIZATION:%{HTTP:Authorization}]
+  RewriteCond %{ENV:REDIRECT_STATUS} ^$
+  RewriteRule ^index\.php(?:/(.*)|$) %{ENV:BASE}/$1 [R=301,L]
+  RewriteCond %{REQUEST_FILENAME} -f
+  RewriteRule ^ - [L]
+  RewriteRule ^ %{ENV:BASE}/index.php [L]
 </Directory>
 ```
--- src/Controller/HKController.php
+++ src/Controller/HKController.php
@@ -2,23 +2,62 @@
 // src/Controller/HKController.php
 namespace App\Controller;
 
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use FOS\RestBundle\Controller\AbstractFOSRestController;
 use FOS\RestBundle\Controller\Annotations as Rest;
+use HKAPI\API as HK;
 
 /**
  * @Route("/hk")
  */
 class HKController extends AbstractFOSRestController
 {
+  private $hk = null;
+  private function getHK()
+  {
+    if ($this->hk == null) {
+      $this->hk = new HK($_ENV['HK_IP'], 10025, new \HKAPI\Devices\AVR());
+    }
+    return $this->hk->zone('Main Zone');
+  }
+
+  private function formatResponse(Request $req, $statusCode, $data = null)
+  {
+    $json = array('uri' => $req->getPathInfo());
+    if ($data != null) $json = array_merge($json, $data);
+    return $this->handleView($this->view($json, $statusCode));
+  }
+
   /**
    * @Rest\Get("/source/{sourceName}")
-   * @return Response
    */
-  public function setSource($sourceName)
+  public function setSource(Request $req, string $sourceName)
   {
-    $number = array('message' => 'coucou ' . $sourceName);
-    return $this->handleView($this->view($number, Response::HTTP_OK));
+    $this->getHK()->selectSource($sourceName);
+    return $this->formatResponse($req, Response::HTTP_OK, array('message' => "Setting source $sourceName..."));
+  }
+
+  /**
+   * @Rest\Get("/off")
+   */
+  public function powerOff(Request $req)
+  {
+    $this->getHK()->off('');
+    return $this->formatResponse($req, Response::HTTP_OK, array('message' => "Powering off..."));
+  }
+
+  /**
+   * @Rest\Get("/volume/{direction}")
+   */
+  public function setVolume(Request $req, string $direction)
+  {
+    if ($direction == 'up') {
+      $this->getHK()->volumeUp('');
+    } else if ($direction == 'down') {
+      $this->getHK()->volumeDown('');
+    }
+    return $this->formatResponse($req, Response::HTTP_OK);
   }
 }
